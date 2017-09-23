7e20666e79f6edf4f2b1b1105a46478376248d2a
photon318 <photon318@gmail.com>
2017-09-23 18:44:46 +0300

    r

--- RBloggers/Maximum Sharpe Portfolio.R
+++ RBloggers/Maximum Sharpe Portfolio.R
@@ -15,19 +15,20 @@ close(con)
 # Create Efficient Frontier
 #******************************************************************
 # create sample historical input assumptions
-ia = az_ia()
+ia = az_ia() # used custom override to use data till 2017 
+# ia = aa.test.create.ia()
 #options(max.print = 10000)
 #ia
-library(ggplot2)
 
-image(ia$correlation, axes = FALSE)
+#t(ia$correlation[rev(order(row.names(ia$correlation))),])
+  
+image(ia$correlation, axes = FALSE) 
+#contour(ia$correlation, axes = FALSE)
+#image(ia$correlation, axes = FALSE)
 axis(3, at =seq(0,1, length = 8), labels = colnames(ia$correlation))
 axis(2, at =seq(0,1, length = 8), labels = rownames(ia$correlation))
 
       
-      aes(x = variable, y = row, fill = value)) + geom_tile() +
-  geom_text(aes(label = value), color = 'white')
-
 # create long-only, fully invested efficient frontier
 n = ia$n
 
@@ -73,6 +74,83 @@ points(
 portfolio.return(weight, ia) /  portfolio.risk(weight, ia)
 
 plota.legend('Minimum Variance,Maximum Sharpe', 'red,orange', x = 'topright')
+
+
+
+#*****************************************************************
+# Examples of Maximum Sharpe or Tangency portfolios construction
+#******************************************************************     
+weight = max.sharpe.portfolio('long-only')(ia,constraints)  
+round(weight,2)
+round(c(sum(weight[weight<0]), sum(weight[weight>0])),2)
+
+weight = max.sharpe.portfolio('long-short')(ia,constraints)         
+round(weight,2)
+round(c(sum(weight[weight<0]), sum(weight[weight>0])),2)
+
+weight = max.sharpe.portfolio('market-neutral')(ia,constraints)         
+round(weight,2)
+round(c(sum(weight[weight<0]), sum(weight[weight>0])),2)  
+
+
+#*****************************************************************
+# Load historical data
+#****************************************************************** 
+load.packages('quantmod')
+
+tickers = spl('SPY,EFA,EWJ,EEM,IYR,RWX,IEF,TLT,DBC,GLD')
+
+data <- new.env()
+getSymbols(tickers, src = 'yahoo', from = '1980-01-01', env = data, auto.assign = T)
+for(i in ls(data)) data[[i]] = adjustOHLC(data[[i]], use.Adjusted=T)                            
+bt.prep(data, align='keep.all', dates='2004:12::')
+
+#*****************************************************************
+# Code Strategies
+#******************************************************************
+prices = data$prices  
+n = ncol(prices)
+
+models = list()
+
+#*****************************************************************
+# Code Strategies
+#******************************************************************
+# find period ends
+period.ends = endpoints(prices, 'months')
+period.ends = period.ends[period.ends > 0]
+
+n.mom = 180
+n.vol = 60
+n.top = 4        
+momentum = prices / mlag(prices, n.mom)  
+
+obj = portfolio.allocation.helper(data$prices, period.ends=period.ends,
+                                  lookback.len = n.vol, universe = ntop(momentum[period.ends,], n.top) > 0,
+                                  min.risk.fns = list(
+                                                      #EW=equal.weight.portfolio
+                                                      #,RP=risk.parity.portfolio
+                                                      #,MV=min.var.portfolio
+                                                      #,MD=max.div.portfolio
+                                                      #MC=min.corr.portfolio
+                                                      #,MC2=min.corr2.portfolio
+                                                      #,MCE=min.corr.excel.portfolio
+                                                      MS=max.sharpe.portfolio()
+                                                      )
+) 
+
+models = create.strategies(obj, data)$models
+
+#*****************************************************************
+# Create Report
+#******************************************************************    
+strategy.performance.snapshoot(models, T)
+
+plotbt.custom.report.part2(models$MS)
+
+# Plot Portfolio Turnover for each strategy
+layout(1)
+barplot.with.labels(sapply(models, compute.turnover, data), 'Average Annual Portfolio Turnover')
 
 
 az_ia <- function()
